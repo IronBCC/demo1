aa39c94da9060d2630ccd3a30b0df2132a1c72bd
ironbcc <ironbcc@gmail.com>
2017-01-16 17:22:08 +0300

    added whole project

--- demo.py
+++ demo.py
@@ -54,16 +54,16 @@ CACHE_FOLDER = "cache/"
 
 Batch = namedtuple('Batch', ['data'])
 
-# CLASSES = ('aeroplane', 'bicycle', 'bird', 'boat',
-#            'bottle', 'bus', 'car', 'cat', 'chair',
-#            'cow', 'diningtable', 'dog', 'horse',
-#            'motorbike', 'person', 'pottedplant',
-#            'sheep', 'sofa', 'train', 'tvmonitor')
-CLASSES = (None, None, None, None,
-           None, None, None, 'cat', 'chair',
-           None, None, 'dog', None,
-           None, 'person', None,
-           None, 'sofa', None, None)
+CLASSES = ('aeroplane', 'bicycle', 'bird', 'boat',
+           'bottle', 'bus', 'car', 'cat', 'chair',
+           'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant',
+           'sheep', 'sofa', 'train', 'tvmonitor')
+# CLASSES = (None, None, None, None,
+#            None, None, None, 'cat', 'chair',
+#            None, None, 'dog', None,
+#            None, 'person', None,
+#            None, 'sofa', None, None)
 
 MAX_IMAGE_SIZE = 700
 SSD_SHAPE_SIZE = 300
@@ -142,14 +142,11 @@ def parse_args():
 
 
 
-def extract_rect(det, width, height, idx = 0, tresh = 0.6, classes = CLASSES):
-    if len(det) < idx + 1 or det[idx, 1] < tresh:
-        return None, None, None
-    class_id = int(det[idx, 0])
-    if not class_id and class_id >= len(classes):
+def extract_rect(det, width, height, tresh = 0.6, classes = CLASSES):
+    class_id = int(det[0])
+    if det[1] < tresh or (not class_id and class_id >= len(classes)):
         return None, None, None
     class_id = classes[class_id]
-    det = det[idx]
     xmin = int(det[2] * width)
     ymin = int(det[3] * height)
     xmax = int(det[4] * width)
@@ -183,10 +180,9 @@ def _npcircle(image, cx, cy, radius, color, transparency=0.0):
         np.array(color).astype('float32') * (1.0 - transparency)).astype('uint8')
 
 def resize(img, size):
-    print img.shape
+    size = (min(size[0], img.shape[0]), min(size[1], img.shape[1]))
     r = img.shape[0] / float(img.shape[1])
     size = (size[0], int(size[0] * r))
-    print  size
     return cv2.resize(img, size)
 
 if __name__ == '__main__':
@@ -211,11 +207,11 @@ if __name__ == '__main__':
     model_bin = 'deepcut-cnn/models/deepercut/ResNet-152.caffemodel'
     scales = '1.'
 
-    align = openface.AlignDlib("openface/models/dlib/shape_predictor_68_face_landmarks.dat")
-    net = openface.TorchNeuralNet(
-        "openface/models/openface/nn4.small2.v1.t7",
-        imgDim=96,
-        cuda=not args.cpu)
+    # align = openface.AlignDlib("openface/models/dlib/shape_predictor_68_face_landmarks.dat")
+    # net = openface.TorchNeuralNet(
+    #     "openface/models/openface/nn4.small2.v1.t7",
+    #     imgDim=96,
+    #     cuda=not args.cpu)
 
     cap = cv2.VideoCapture("test2.mp4")
     idx = 0
@@ -225,14 +221,15 @@ if __name__ == '__main__':
     print("Loaded")
     while (True):
         ret, img_origin_size = cap.read()
-        # img_origin_size = cv2.imread("image.png")
+        # img_origin_size = cv2.imread("two_person.png")
         # img_origin_size = image = scipy.misc.imread("image.png")
-        # ret= True
+        ret= True
         print("Video read, ", ret)
         if(img_origin_size is None):
             print "Unable to read image"
             break
 
+        img_origin_size = resize(img_origin_size, (MAX_IMAGE_SIZE, MAX_IMAGE_SIZE))
         img_origin_size = cv2.flip(img_origin_size, -1)
         img_origin = resize(img_origin_size, (SSD_SHAPE_SIZE, SSD_SHAPE_SIZE))
         cache_img = CACHE_FOLDER + "cache.jpg"
@@ -251,11 +248,10 @@ if __name__ == '__main__':
         last_pt2 = None
         last_cl = None
         for det in dets:
-            pt1, pt2, cl = extract_rect(dets, img_origin_size.shape[1], img_origin_size.shape[0])
+            pt1, pt2, cl = extract_rect(det, img_origin_size.shape[1], img_origin_size.shape[0])
             if cl is None or \
                     (last_pt1 == pt1 and last_pt2 == pt2 and last_cl == cl):
                 continue
-
             if cl != "person" :
                 continue
 
@@ -276,7 +272,7 @@ if __name__ == '__main__':
             else:
                 image = image[:, :, ::-1]
             print(" pose progress ", image.shape)
-            pose = estimate_pose(image, model_def, model_bin, [.8])
+            pose = estimate_pose(image, model_def, model_bin, [1.])
 
             print("pose : ", (time.clock() - pose_start))
             print("img_preprocessing : ", (time.clock() - start))
@@ -296,11 +292,11 @@ if __name__ == '__main__':
                                   colors[p_idx],
                                   0.0)
                     vis_name = person_out_name + '_vis.jpg'
-                    cv2.imwrite(vis_name, visim)
+                    # cv2.imwrite(vis_name, visim)
                 except Exception:
                     print "Ignore"
 
-                img_origin_size = cv2.rectangle(img_origin_size, pt1, pt2, (255, 0, 0))
+                cv2.rectangle(img_origin_size, pt1, pt2, (255, 0, 0))
 
             with open(person_deepcut_out_name, 'wb') as outfile:
                 json.dump(pose.tolist(), outfile)
@@ -311,6 +307,7 @@ if __name__ == '__main__':
         # time.sleep(1)
         # Display the resulting frame
         # cv2.imshow('frame', gray)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
